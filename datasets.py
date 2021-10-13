fe30492c53248c5c94d0e2009f505f4699d59c7a
chashu2464 <47881538+chashu2464@users.noreply.github.com>
2021-10-14 00:00:59 +0800

    modify

--- datasets.py
+++ datasets.py
@@ -7,17 +7,17 @@ from PIL import Image
 import torchvision.transforms as transforms
 
 class ImageDataset(Dataset):
-    def __init__(self, root, transforms_=None, unaligned=False, mode='train'):
+    def __init__(self, root, transforms_=None, random=False, mode='train'):
         self.transform = transforms.Compose(transforms_)
-        self.unaligned = unaligned
+        self.random = random
 
-        self.files_A = sorted(glob.glob(os.path.join(root, '%s/A' % mode) + '/*.*'))
-        self.files_B = sorted(glob.glob(os.path.join(root, '%s/B' % mode) + '/*.*'))
+        self.files_A = sorted(glob.glob(os.path.join(root, '%s/publish' % mode) + '/*.*'))
+        self.files_B = sorted(glob.glob(os.path.join(root, '%s/contrast' % mode) + '/*.*'))
 
     def __getitem__(self, index):
         item_A = self.transform(Image.open(self.files_A[index % len(self.files_A)]))
 
-        if self.unaligned:
+        if self.random:
             item_B = self.transform(Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)]))
         else:
             item_B = self.transform(Image.open(self.files_B[index % len(self.files_B)]))
